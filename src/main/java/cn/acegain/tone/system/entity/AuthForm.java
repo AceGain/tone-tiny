c6e47ec13925a7710e8ddffdd167e1885e613484
acegain <acegain@163.com>
2026-01-29 11:55:19 +0800

    调整认证表单。

--- src/main/java/cn/acegain/tone/system/entity/AuthForm.java
+++ src/main/java/cn/acegain/tone/system/entity/AuthForm.java
@@ -3,11 +3,15 @@ package cn.acegain.tone.system.entity;
 import lombok.Data;
 
 @Data
-public class AuthAo {
+public class AuthForm {
 
     private String uuid;
 
     private String code;
+
+    private String channel;
+
+    private String mode;
 
     private String account;
 
